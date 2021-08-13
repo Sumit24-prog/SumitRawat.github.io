6096a3402b7ad1f7135a95effe9e3aba320ce71f
Sumit24-prog <rawatsingh2112singh@gmail.com>
2021-08-13 11:59:00 +0530

    javascript added

--- Js/project.js
+++ Js/project.js
@@ -12,3 +12,33 @@ setInterval(() => {
     mn.style.transform = `rotateZ(${mm}deg)`;
     sc.style.transform = `rotateZ(${ss}deg)`;
 })
+
+let p;
+let c=0;
+
+function change(p) { 
+    if (o > 0) {
+        let oe = document.getElementById('pr' + o).style;
+        oe.height = '200px';
+        oe.top = '200px';
+    }
+    if (c > 0) {
+        let te = document.getElementById('pr' + c).style;
+        te.left = '-500px';
+    }
+    let elid = 'pr';
+    elid = elid + p;
+    let el = document.getElementById(elid).style;
+    el.left = '500px';
+    el.top = '200px';
+    c = p;
+}
+let n;
+let o=0;
+
+function more(n) {
+    let info = document.getElementById('pr' + n).style;
+    info.top = '80px';
+    info.height = '400px'; 
+    o = n;
+}
